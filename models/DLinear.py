d3fc4f26df8eca3d3c6127ec8346b0d217049881
kevin-aron <1982153303@qq.com>
2023-12-02 18:13:41 +0800

    thorough repair

---
+++ models/DLinear.py
@@ -0,0 +1,95 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+class moving_avg(nn.Module):
+    """
+    Moving average block to highlight the trend of time series
+    """
+    def __init__(self, kernel_size, stride):
+        super(moving_avg, self).__init__()
+        self.kernel_size = kernel_size
+        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
+
+    def forward(self, x):
+        # padding on the both ends of time series
+        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        x = torch.cat([front, x, end], dim=1)
+        x = self.avg(x.permute(0, 2, 1))
+        x = x.permute(0, 2, 1)
+        return x
+
+
+class series_decomp(nn.Module):
+    """
+    Series decomposition block
+    """
+    def __init__(self, kernel_size):
+        super(series_decomp, self).__init__()
+        self.moving_avg = moving_avg(kernel_size, stride=1)
+
+    def forward(self, x):
+        # 滑动平均
+        moving_mean = self.moving_avg(x)
+        # 季节趋势性
+        res = x - moving_mean
+        return res, moving_mean
+
+class Model(nn.Module):
+    """
+    Decomposition-Linear
+    """
+    def __init__(self, configs):
+        super(Model, self).__init__()
+        self.seq_len = configs.seq_len
+        self.pred_len = configs.pred_len
+
+        # Decompsition Kernel Size
+        kernel_size = 25
+        self.decompsition = series_decomp(kernel_size)
+        self.individual = configs.individual
+        self.channels = configs.enc_in
+
+        if self.individual:
+            self.Linear_Seasonal = nn.ModuleList()
+            self.Linear_Trend = nn.ModuleList()
+            
+            for i in range(self.channels):
+                self.Linear_Seasonal.append(nn.Linear(self.seq_len,self.pred_len))
+                self.Linear_Trend.append(nn.Linear(self.seq_len,self.pred_len))
+
+                # Use this two lines if you want to visualize the weights
+                # self.Linear_Seasonal[i].weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
+                # self.Linear_Trend[i].weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
+        else:
+            self.Linear_Seasonal = nn.Linear(self.seq_len,self.pred_len)
+            self.Linear_Trend = nn.Linear(self.seq_len,self.pred_len)
+            
+            # Use this two lines if you want to visualize the weights
+            # self.Linear_Seasonal.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
+            # self.Linear_Trend.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
+
+    def forward(self, x):
+        # x: [Batch, Input length, Channel]
+        # 季节与时间趋势性分解
+        seasonal_init, trend_init = self.decompsition(x)
+        # 将维度索引2与维度索引1交换
+        seasonal_init, trend_init = seasonal_init.permute(0,2,1), trend_init.permute(0,2,1)
+        if self.individual:
+            seasonal_output = torch.zeros([seasonal_init.size(0),seasonal_init.size(1),self.pred_len],dtype=seasonal_init.dtype).to(seasonal_init.device)
+            trend_output = torch.zeros([trend_init.size(0),trend_init.size(1),self.pred_len],dtype=trend_init.dtype).to(trend_init.device)
+            for i in range(self.channels):
+                # 使用全连接层得到季节性
+                seasonal_output[:,i,:] = self.Linear_Seasonal[i](seasonal_init[:,i,:])
+                # 使用全连接层得到趋势性
+                trend_output[:,i,:] = self.Linear_Trend[i](trend_init[:,i,:])
+                # 两者共享所有权重
+        else:
+            seasonal_output = self.Linear_Seasonal(seasonal_init)
+            trend_output = self.Linear_Trend(trend_init)
+        # 将季节性与趋势性相加
+        x = seasonal_output + trend_output
+        # 交换维度位置
+        return x.permute(0,2,1) # to [Batch, Output length, Channel]
---
+++ run_longExp.py
@@ -0,0 +1,158 @@
+import argparse
+import os
+import torch
+from exp.exp_main import Exp_Main
+import random
+import numpy as np
+
+fix_seed = 2021
+random.seed(fix_seed)
+torch.manual_seed(fix_seed)
+np.random.seed(fix_seed)
+
+
+parser = argparse.ArgumentParser(description='Autoformer & Transformer family for Time Series Forecasting')
+
+# 是否进行训练
+parser.add_argument('--is_training', type=int, default=1, help='status')
+# 模型前缀
+parser.add_argument('--model_id', type=str, default='test', help='model id')
+# 选择模型(可选模型有Autoformer, Informer, Transformer，DLinear，NLinear)
+parser.add_argument('--model', type=str, default='DLinear',
+                    help='model name, options: [Autoformer, Informer, Transformer]')
+
+# 数据选择
+parser.add_argument('--data', type=str, default='ETTh1', help='dataset type')
+# 数据存放路径
+parser.add_argument('--root_path', type=str, default='K:/LLinear/data/', help='root path of the data file')
+# 数据完整名称
+parser.add_argument('--data_path', type=str, default='ETTh1.csv', help='data file')
+# 预测类型(多变量预测、单变量预测、多元预测单变量)
+parser.add_argument('--features', type=str, default='M',
+                    help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, S:univariate predict univariate, MS:multivariate predict univariate')
+# 如果选择单变量预测或多元预测单变量，需要指定预测的列
+parser.add_argument('--target', type=str, default='OT', help='target feature in S or MS task')
+# 数据重采样格式
+parser.add_argument('--freq', type=str, default='h',
+                    help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h')
+# 模型存放文件夹
+parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
+
+# 时间窗口长度
+parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
+# 先验序列长度
+parser.add_argument('--label_len', type=int, default=48, help='start token length')
+# 要预测的序列长度
+parser.add_argument('--pred_len', type=int, default=96, help='prediction sequence length')
+
+
+# 针对DLinear是否为每个变量（通道）单独建立一个线性层
+parser.add_argument('--individual', action='store_true', default=False, help='DLinear: a linear layer for each variate(channel) individually')
+
+# 嵌入策略选择
+parser.add_argument('--embed_type', type=int, default=0, help='0: default 1: value embedding + temporal embedding + positional embedding 2: value embedding + temporal embedding 3: value embedding + positional embedding 4: value embedding')
+# 编码器default参数为特征列数
+parser.add_argument('--enc_in', type=int, default=7, help='encoder input size') # DLinear with --individual, use this hyperparameter as the number of channels
+# 解码器default参数与编码器相同
+parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
+parser.add_argument('--c_out', type=int, default=7, help='output size')
+# 模型宽度
+parser.add_argument('--d_model', type=int, default=512, help='dimension of model')
+# 多头注意力机制头数
+parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
+# 模型中encoder层数
+parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
+# 模型中decoder层数
+parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
+# 全连接层神经元个数
+parser.add_argument('--d_ff', type=int, default=2048, help='dimension of fcn')
+# 窗口平均线的窗口大小
+parser.add_argument('--moving_avg', type=int, default=25, help='window size of moving average')
+# 采样因子数
+parser.add_argument('--factor', type=int, default=1, help='attn factor')
+# 是否需要序列长度衰减
+parser.add_argument('--distil', action='store_false',
+                    help='whether to use distilling in encoder, using this argument means not using distilling',
+                    default=True)
+# drop_out率
+parser.add_argument('--dropout', type=float, default=0.05, help='dropout')
+# 时间特征编码方式
+parser.add_argument('--embed', type=str, default='timeF',
+                    help='time features encoding, options:[timeF, fixed, learned]')
+# 激活函数
+parser.add_argument('--activation', type=str, default='gelu', help='activation')
+# 是否输出attention
+parser.add_argument('--output_attention', action='store_true', help='whether to output attention in ecoder')
+# 是否进行预测
+parser.add_argument('--do_predict', action='store_false', help='whether to predict unseen future data')
+
+# 多线程
+parser.add_argument('--num_workers', type=int, default=0, help='data loader num workers')
+# 训练轮数
+parser.add_argument('--itr', type=int, default=1, help='experiments times')
+# 训练迭代次数
+parser.add_argument('--train_epochs', type=int, default=100, help='train epochs')
+# batch_size大小
+parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
+# early stopping检测间隔
+parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
+# 学习率
+parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
+parser.add_argument('--des', type=str, default='test', help='exp description')
+# loss函数
+parser.add_argument('--loss', type=str, default='mse', help='loss function')
+# 学习率衰减参数
+parser.add_argument('--lradj', type=str, default='type1', help='adjust learning rate')
+# 是否使用自动混合精度训练
+parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
+
+# 是否使用GPU训练
+parser.add_argument('--use_gpu', type=bool, default=False, help='use gpu')
+parser.add_argument('--gpu', type=int, default=0, help='gpu')
+# GPU分布式训练
+parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
+# 多GPU训练
+parser.add_argument('--devices', type=str, default='0,1,2,3', help='device ids of multile gpus')
+parser.add_argument('--test_flop', action='store_true', default=False, help='See utils/tools for usage')
+
+# 取参数表
+args = parser.parse_args()
+# 获取GPU
+args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
+
+if args.use_gpu and args.use_multi_gpu:
+    args.dvices = args.devices.replace(' ', '')
+    device_ids = args.devices.split(',')
+    args.device_ids = [int(id_) for id_ in device_ids]
+    args.gpu = args.device_ids[0]
+
+print('Args in experiment:')
+print(args)
+
+Exp = Exp_Main
+
+if args.is_training:
+    for ii in range(args.itr):
+        # setting record of experiments
+        setting = '{}_rate {}'.format(args.model, args.learning_rate)
+
+        exp = Exp(args)  # set experiments
+        print('>>>>>>>start training : {}>>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
+        exp.train(setting)
+
+        print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+        exp.test(setting)
+
+        if args.do_predict:
+            print('>>>>>>>predicting : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+            exp.predict(setting, True)
+
+        torch.cuda.empty_cache()
+else:
+    ii = 0
+    setting = '{}_model'.format(args.model)
+
+    exp = Exp(args)  # set experiments
+    print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+    exp.test(setting, test=1)
+    torch.cuda.empty_cache()
